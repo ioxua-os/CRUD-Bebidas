28c5225b971999e73757f8511822715d73a0e575
Yehoshua Oliveira <oliveira@e7code.com.br>
2017-09-18 18:10:07 -0300

    feature: adicionado modal de exclusão de bebidas

--- routes/bebidas.js
+++ routes/bebidas.js
@@ -11,31 +11,52 @@ var format = formatNumber({
 });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	var listagem = [
-			{
-				nome: "Nome Legal",
-				tipo: "Whiskey",
-				marca: "Johnny Walker",
-				ano: 1990,
-				preco: 70
-			},
-			{
-				nome: "Original",
-				tipo: "Vinho Tinto",
-				marca: "Periquita",
-				ano: 2013,
-				preco: 38.8
-			}
-	];
+router.route('/')
+	.get((req, res, next) => {
+		var listagem = [
+				{
+					id: 1,
+					nome: "Nome Legal",
+					tipo: "Whiskey",
+					marca: "Johnny Walker",
+					ano: 1990,
+					preco: 70
+				},
+				{
+					id: 2,
+					nome: "Original",
+					tipo: "Vinho Tinto",
+					marca: "Periquita",
+					ano: 2013,
+					preco: 38.8
+				}
+		];
 
-	listagem.forEach((bebida, index) => {
-		bebida.preco = format(bebida.preco)
-	});
+		listagem.forEach((bebida, index) => {
+			bebida.preco = format(bebida.preco)
+		})
 
-	res.render('all_bebidas', {
-		title: "Listagem",
-		bebidas: listagem
+		res.render('all_bebidas', {
+			title: "Listagem",
+			bebidas: listagem
+		})
+	})
+
+router.route('/:id')
+	.get((req, res) => {
+		res.render('todo', {
+			message: 'Exibir a view de detalhes de uma bebida'
+		});
+	})
+	.delete((req, res) => { // DELETE '/'
+		res.render('todo', {
+			message: 'Deletar o bagulho'
+		})
+	})
+
+router.post('/new', (req, res) => {
+	res.render('todo', {
+		message: 'Cadastrar a bebida'
 	});
 });
 
@@ -45,10 +66,16 @@ router.get('/create', (req, res) => {
 	});
 });
 
-router.post('/new', (req, res) => {
-	res.render('todo', {
-		message: 'Cadastrar a bebida'
+router.route('/edit/:id')
+	.get((req, res) => {
+		res.render('todo', {
+			message: 'Exibir a view de edição'
+		});
+	})
+	.post((req, res) => {
+		res.render('todo', {
+			message: 'Editar a bebida na persistência'
+		});
 	});
-});
 
 module.exports = router;
