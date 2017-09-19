e5ed4ab27a8280913bfba859e2a3f8a2a5693006
Yehoshua Oliveira <oliveira@e7code.com.br>
2017-09-19 14:42:21 -0300

    fix: removido o mapeamento para a index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CRUD Bebidas' });
+	res.redirect('/bebidas')
 });
 
 module.exports = router;
